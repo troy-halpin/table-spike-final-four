42f639326f12ff29f8261ca31a1bf7d83a1c1161
Troy Halpin <troy.halpin@procore.com>
2020-06-15 01:39:36 -0700

    use custom cell renderer in devextreme table

--- src/dx-react-grid/DXReactGridTable.js
+++ src/dx-react-grid/DXReactGridTable.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
+import { Font } from '@procore/core-react'
 import {
   Grid,
   VirtualTable,
   TableHeaderRow,
 } from '@devexpress/dx-react-grid-bootstrap4'
+import { DataTypeProvider } from '@devexpress/dx-react-grid'
 import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css'
 
 import { generateRows, defaultColumnValues } from './generator'
+
+const CityFormatter = ({ value }) => <Font weight="bold">{value}</Font>
+
+const CityTypeProvider = (props) => (
+  <DataTypeProvider formatterComponent={CityFormatter} {...props} />
+)
 
 const getRowId = (row) => row.id
 
@@ -18,6 +26,9 @@ export function DXReactGridTable() {
     { name: 'city', title: 'City' },
     { name: 'car', title: 'Car' },
   ])
+
+  const [cityColumns] = React.useState(['city'])
+
   const [rows] = useState(
     generateRows({
       columnValues: { id: ({ index }) => index, ...defaultColumnValues },
@@ -28,6 +39,7 @@ export function DXReactGridTable() {
   return (
     <div className="card">
       <Grid rows={rows} columns={columns} getRowId={getRowId}>
+        <CityTypeProvider for={cityColumns} />
         <VirtualTable />
         <TableHeaderRow />
       </Grid>
